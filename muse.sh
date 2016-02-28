d724b973930cabe63993d072a6dba1648408922f
mihaiolteanu <mihai_olteanu@fastmail.fm>
2016-02-28 13:59:11 +0200

    Add Top Tracks support

--- muse.sh
+++ muse.sh
@@ -14,44 +14,39 @@ done
 [[ -f ~/.muserc ]] || { echo >&2 "~/.muserc does not exist"; exit 1; }
 source ~/.muserc
 
+
 lastfm_base_req="https://ws.audioscrobbler.com/2.0/?format=json&api_key=${LASTFM_API_KEY}"
 lastfm_artist_req=$lastfm_base_req"&method=artist.getInfo&artist="     # ${artist} to be added
 lastfm_toptracks_req=$lastfm_base_req"&method=artist.getTopTracks&limit=${LASTFM_TOP_TRACKS_NO}&artist=" # ${artist} to be added
+
+youtube="https://www.youtube.com/"
+youtube_search=$youtube"results?search_query="
 
 echo_underline () { echo -ne "\e[4m$@\e[0m"; }
 echo_bold () { echo -ne "\e[1m$@\e[0m"; }
 tags_info () { echo "Not implemented yet. Exiting.."; exit 0; }
 
-artist_top_tracks () {
-    declare lastfm_resp=`curl --silent $lastfm_toptracks_req${artist}`
-    declare toptracks=`echo $lastfm_resp | jq -r '.toptracks.track | .[] | .name' | tr '\n' ','`
-
-    echo_bold "Top tracks: "; echo
-    oldIFS=$IFS; IFS=$','
-    PS3="Listen to: "
-    select entry in $toptracks "Quit"; do
-        [[ $entry = "Quit" ]] && exit 0
-        declare song=`echo $entry | tr ' ' '+'`
-        youtube-dl -o - www.youtube.com/$(curl -s https://www.youtube.com/results\?search_query\=${artist}+${song} | grep -o 'watch?v=[^"]*"[^>]*title="[^"]*' | head -n 1 | awk '{print $1;}' | sed 's/*//') | vlc - &
-        exit 0
-    done
+artist_toptracks_get () {
+    lastfm_resp=$(curl --silent $lastfm_toptracks_req${artist})
+    toptracks=$(echo $lastfm_resp | jq -r '.toptracks.track | .[] | .name' | tr '\n' ',')
 }
 
 # get all the info from last.fm for the artist given as $1 parameter
 artist_info_get () {
     artist_raw=$1                                            # artist name, as given by the user
-    artist=`echo $artist_raw | tr ' ' '+'`                   # artist name, suitable for last.fm query
-    lastfm_resp=`curl --silent $lastfm_artist_req${artist}`  # get json response from last.fm
-    similar=`echo $lastfm_resp | jq -r '.artist.similar.artist | .[] | .name' | tr '\n' ','`
-    summary=`echo $lastfm_resp | jq -r '.artist.bio.summary'; echo`
-    tags=`echo $lastfm_resp | jq -r '.artist.tags.tag | .[] | .name' | tr '\n' ','`
+    artist=$(echo $artist_raw | tr ' ' '+')                  # artist name, suitable for last.fm query
+    lastfm_resp=$(curl --silent $lastfm_artist_req${artist})  # get json response from last.fm
+    similar=$(echo $lastfm_resp | jq -r '.artist.similar.artist | .[] | .name' | tr '\n' ',')
+    summary=$(echo $lastfm_resp | jq -r '.artist.bio.summary'; echo)
+    tags=$(echo $lastfm_resp | jq -r '.artist.tags.tag | .[] | .name' | tr '\n' ',')
 }
 
 artist_info_display () {
     # display the common artist info. this info doesn't change when the
     # selection menu changes
     clear
-    echo_bold $artist_raw": "; echo $summary; echo
+    echo_bold $artist_raw": "
+    echo ${summary%%<a href*}; echo # remove link at the end of summary
     echo_bold "Tags: "; echo $tags; echo
     echo_bold "Similar artists: "; echo $similar; echo
 
@@ -67,26 +62,46 @@ artist_info_display () {
                     "Similar artists")
                         artist_info_display "Similar artists" ;;
                     "Tags")
-                        select choice in $tags "Quit"; do
-                            [[ $choice = "Quit" ]] && exit 0
-                            tags_info "$choice"
-                        done ;;
+                        artist_info_display "Tags" ;;
                     "Top Tracks")
-                        artist_top_tracks "$artist_raw" ;;
+                        artist_toptracks_get "$artist_raw"
+                        artist_info_display "Top Tracks" ;;
                 esac
                 exit 0
             done ;;
         "Similar artists")
             PS3="Pick an artist: "
             oldIFS=$IFS; IFS=$','
-            select choice in $similar "Quit"; do
+            select choice in $similar "Go Back" "Quit"; do
                 [[ $choice = "Quit" ]] && exit 0
                 IFS=$oldIFS
+                [[ $choice = "Go Back" ]] &&  artist_info_display "Explore"
                 artist_info_get "$choice"
                 artist_info_display "Explore"
             done ;;
+        "Tags")
+            PS3="Pick a tag: "
+            oldIFS=$IFS; IFS=$','
+            select choice in $tags "Go Back" "Quit"; do
+                [[ $choice = "Quit" ]] && exit 0
+                IFS=$oldIFS
+                [[ $choice = "Go Back" ]] &&  artist_info_display "Explore"
+                tags_info "$choice"
+            done ;;
+        "Top Tracks")
+            echo_bold "Top Tracks: "; echo
+            PS3="Listen to: "
+            oldIFS=$IFS; IFS=$','
+            select choice in $toptracks "Go Back" "Quit"; do
+                [[ $choice = "Quit" ]] && exit 0
+                IFS=$oldIFS
+                [[ $choice = "Go Back" ]] &&  artist_info_display "Explore"
+                song=$(echo $choice | tr ' ' '+')
+                echo $youtube_search${artist}+${song}
+                youtube-dl 2>/dev/null -o - $youtube$(curl -s $youtube_search${artist}+${song} | grep -o 'watch?v=[^"]*"[^>]*title="[^"]*' | head -n 1 | awk '{print $1;}' | sed 's/*//') | vlc >/dev/null 2>&1 - &
+            done
     esac
 }
 
-artist_info_get "lacuna coil"
+artist_info_get "$1"
 artist_info_display "Explore"
